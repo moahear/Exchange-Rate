dcf6bedfbb6198737a5afe61b61280c781b12d6b
Mostafa Ahmadi <moahear@gmail.com>
2023-04-20 00:39:29 +0430

    design activity_market

---
+++ app/src/main/java/com/gamil/moahear/exchangerate/CoinFragment.kt
@@ -0,0 +1,27 @@
+package com.gamil.moahear.exchangerate
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.gamil.moahear.exchangerate.databinding.FragmentCoinBinding
+
+class CoinFragment:Fragment() {
+    private lateinit var binding:FragmentCoinBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding=FragmentCoinBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+
+        }
+    }
+}
--- app/src/main/java/com/gamil/moahear/exchangerate/MarketActivity.kt
+++ app/src/main/java/com/gamil/moahear/exchangerate/MarketActivity.kt
@@ -2,13 +2,13 @@ package com.gamil.moahear.exchangerate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.gamil.moahear.exchangerate.databinding.ActivityMainBinding
+import com.gamil.moahear.exchangerate.databinding.ActivityMarketBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class MarketActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMarketBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMarketBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.apply {
 
